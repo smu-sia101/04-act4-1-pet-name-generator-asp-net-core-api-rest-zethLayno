e7401fea70d4ea40a8b7c70f261cd81771585bc0
ZETHLAURENCELAYNO <ZETHLAURENCELAYNO@SMU-0046>
2025-03-25 17:50:19 +0800

    zekko pogi

---
+++ PetNameGenerator.API/Constants/Names.cs
@@ -0,0 +1,12 @@
+﻿namespace PetNameGenerator.API.Constants
+{
+    public class Names
+    {
+        private static readonly Dictionary<string, List<string>> PetNames = new()
+        {
+            { "dog", new List<string> { "KOA", "CHICHI", "LUNA", "WOLFHOUND", "PIPOY" } },
+            { "cat", new List<string> { "PUSSY", "KURSING", "MUSANG", "KITTY", "PETCHAY" } },
+            { "bird", new List<string> { "BIRDYLOG", "TUKA", "BILLIT", "RAVEN", "QUIN" } }
+        };
+    }
+}
